1423ed39e7f890850f63fb2f334c82829a49c28e
Kailin Dong <kailind@andrew.cmu.edu>
2018-12-03 00:52:49 -0500

    push final

---
+++ Assets/javascript/main.js
@@ -0,0 +1,125 @@
+window.sr = ScrollReveal({ reset: true });
+
+
+
+
+// Custom Settings
+sr.reveal('#fistsGunTitle', {
+    distance: '30%',
+    origin: 'bottom',
+    opacity: null,
+    duration:1400
+});
+sr.reveal('#fists_and_guns_intro', {
+    distance: '50%',
+    origin: 'bottom',
+    opacity: null,
+    duration:1400
+});
+
+sr.reveal('.navbar-fixed-top',{duration:1500});
+
+sr.reveal('.animation_1', {
+    distance: '10%',
+    origin: 'bottom',
+    opacity: 0.4,
+    duration:1500
+});
+
+
+sr.reveal('.animation_2', {
+    distance: '10%',
+    origin: 'bottom',
+    opacity: 0.4,
+    duration:1500
+});
+
+
+// sr.reveal('#mainbody_1_2', {
+//     distance: '10%',
+//     origin: 'bottom',
+//     opacity: 0.4,
+//     duration:1500
+// });
+
+sr.reveal('.textSpecial', {
+    distance: '10%',
+    origin: 'bottom',
+    opacity: 0.4,
+    duration:1500
+});
+
+sr.reveal('.animation_4', {
+    distance: '10%',
+    origin: 'bottom',
+    opacity: 0.4,
+    duration:1500
+});
+
+// sr.reveal('.textBody', {
+//     distance: '10%',
+//     origin: 'bottom',
+//     opacity: 0.4,
+//     duration:1500
+// });
+
+sr.reveal('#mainDiv', {
+    distance: '20%',
+    origin: 'bottom',
+    opacity: 0.6,
+    duration:1200
+});
+
+
+sr.reveal('#aboutDiv', {
+    distance: '20%',
+    origin: 'bottom',
+    opacity: 0.6,
+    duration:1200
+});
+
+sr.reveal('#summary', {
+    distance: '40%',
+    origin: 'bottom',
+    opacity: 0.6,
+    duration:1200
+});
+
+sr.reveal('#last_nav_1', {
+    distance: '40%',
+    origin: 'top',
+    opacity: 0,
+    duration:800
+});
+
+sr.reveal('#last_nav_2', {
+    distance: '40%',
+    origin: 'top',
+    opacity: 0,
+    duration:1200,
+    delay:100
+});
+
+
+sr.reveal('#last_nav_3', {
+    distance: '40%',
+    origin: 'top',
+    opacity: 0,
+    duration:1600,
+    delay:200
+});
+
+
+sr.reveal('#last_nav_4', {
+    distance: '40%',
+    origin: 'top',
+    opacity: 0,
+    duration:2000,
+    delay:300
+});
+
+// function showText(){
+// 	document.getElementById("rightSub_1").a.style.display="block";
+// };
+
+
